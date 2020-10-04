19d9a56c692ab9b139e0558580f282e43540ac59
Alexandre Monteiro <fc51023@alunos.fc.ul.pt>
2020-10-04 22:56:40 +0100

    A navegação foi implementada

--- app/src/main/java/com/example/paint/TelaActivity.java
+++ app/src/main/java/com/example/paint/TelaActivity.java
@@ -1,8 +1,13 @@
 package com.example.paint;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class TelaActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class TelaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.tela_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent1 = new Intent(this, Settings.class);
+                startActivity(intent1);
+                return true;
+            case R.id.about:
+                Intent intent2 = new Intent(this, About.class);
+                startActivity(intent2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
