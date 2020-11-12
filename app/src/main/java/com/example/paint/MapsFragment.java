6ae170c997f2a6b8ae5d4f9c9b747f9faaa876d8
Alexandre Monteiro <fc51023@alunos.fc.ul.pt>
2020-11-12 15:00:32 +0000

    Ja funciona!!!

--- app/src/main/java/com/example/paint/MapsFragment.java
+++ app/src/main/java/com/example/paint/MapsFragment.java
@@ -3,10 +3,9 @@ package com.example.paint;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,24 +17,31 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
+    public Polyline line;
     boolean drawmode = false;
-
+    private boolean requestingLocationUpdates = true;
+    private LocationCallback locationCallback;
     private LocationRequest locReq;
     private FusedLocationProviderClient fusedLocationClient;
-    private LocationManager locMan;
-    private LocationListener locLis;
-    private PolyCanvas canvas;
+    private GoogleMap mMap;
 
     @Nullable
     @Override
@@ -43,21 +49,14 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_maps, container, false);
-        this.fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
-        this.locMan = (LocationManager) getActivity().getSystemService(getActivity().LOCATION_SERVICE);
-        this.locReq = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(10 * 1000)
-                .setFastestInterval(1 * 1000);
-
         final Button b = v.findViewById(R.id.mapdrawbutton);
         b.setText(getResources().getString(drawmode ? R.string.stopdraw : R.string.startdraw));
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                drawmode = drawmode ? false : true;
-                if(!drawmode){
-                    canvas.startNewPolyline();
+                drawmode = !drawmode;
+                if (!drawmode) {
+
                 }
                 b.setText(getResources().getString(drawmode ? R.string.stopdraw : R.string.startdraw));
             }
@@ -82,31 +81,50 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 getActivity().requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}, 10);
             }
-            return;
         }
-        this.canvas = new PolyCanvas(googleMap);
+        googleMap.setMinZoomPreference(16.0f);
+        googleMap.setMaxZoomPreference(18.0f);
+        googleMap.setMyLocationEnabled(true);
+        this.fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        this.locReq = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(5000)
+                .setFastestInterval(1000);
+        this.mMap = googleMap;
         fusedLocationClient.getLastLocation()
                 .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
                         if (location != null) {
                             LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
-                            googleMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
 
                         }
                     }
                 });
-        googleMap.setMinZoomPreference(16.0f);
-        googleMap.setMaxZoomPreference(18.0f);
-        googleMap.setMyLocationEnabled(true);
-        this.locLis = new LocationListener() {
+        locationCallback = new LocationCallback() {
             @Override
-            public void onLocationChanged(@NonNull Location location) {
-                LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
-                canvas.drawPolyline(loc);
-                googleMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+                    if (line == null) {
+                        line = mMap.addPolyline(new PolylineOptions().add(loc));
+                    }
+                    if (drawmode) {
+                        if (line == null) {
+                            line = mMap.addPolyline(new PolylineOptions().add(loc));
+                        } else {
+                            List<LatLng> points = new ArrayList<>(line.getPoints());
+                            points.add(loc);
+                            line.setPoints(points);
+                        }
+                    }
+                }
             }
         };
-        this.locMan.requestLocationUpdates("gps", 1000, 0, this.locLis);
+        fusedLocationClient.requestLocationUpdates(this.locReq, locationCallback, Looper.getMainLooper());
     }
 }
